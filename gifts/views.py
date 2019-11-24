1a0952c150f1a0b5922b1a6a3b2fb57a4ad8efc9
Justyna Kędzierska <jus.kedzierska@gmail.com>
2019-11-24 15:46:31 +0100

    Added 'donation_count' and 'institution_count'

--- gifts/views.py
+++ gifts/views.py
@@ -1,11 +1,20 @@
+from django.db.models import Sum, Count, FloatField
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
 
+from gifts.models import Donation
+
 
 class LandingPageViews(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LandingPageViews, self).get_context_data(**kwargs)
+        context['donation_count'] = Donation.objects.aggregate(sum_quantity=Sum('quantity'))
+        context['institution_count'] = Donation.objects.aggregate(sum_institution=Count('institution', distinct=True)) # pobiera unikalne
+        return context
 
 
 class LoginView(TemplateView):
