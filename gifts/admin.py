6ccc30eea36f15853ad4bfc654b5d5e3d2ca4ed3
Justyna Kędzierska <jus.kedzierska@gmail.com>
2019-12-01 20:18:25 +0100

    Added administrator home page

--- gifts/admin.py
+++ gifts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from gifts.models import Category, Institution, Donation
+
+admin.site.register(Category)
+admin.site.register(Institution)
+admin.site.register(Donation)
+# #register link
+# admin.site.site_url = 'admin/'
